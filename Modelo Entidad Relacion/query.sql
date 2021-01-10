2ca42af0616d55fdf86e887a17a3e6235fe5259b
ccontreras@cobiscorp.com <ccontreras@cobiscorp.com>
2021-01-10 18:24:07 -0500

    second commit

--- Modelo Entidad Relacion/query.sql
+++ Modelo Entidad Relacion/query.sql
@@ -1,7 +1,1 @@
-select vh.placa, td.documento, emp.numero_identificacion, emp.nombre_empresa, count(vhc.conductores_idconductores) from vehiculos as vh
-inner join empresas as emp on emp.idempresa=vh.empresas_idempresas
-inner join tipo_documento as td on td.id_tipo_documento= emp.tipo_documento_idtipodocumento
-inner join vehiculos_has_conductores as vhc on vhc.vehiculos_idvehiculos = vh.idvehiculos
-group by vhc.conductores_idconductores,vh.placa, td.documento, emp.numero_identificacion, emp.nombre_empresa
-having count(vhc.conductores_idconductores)>2
-order by vh.placa
+select vh.placa, td.documento, emp.numero_identificacion, emp.nombre, count(vhc.conductores_idconductores) from vehiculos as vh inner join empresas as emp on emp.idempresas=vh.empresas_idempresas inner join tipo_documento as td on td.idtipo_documento= emp.tipo_documento_idtipo_documento inner join vehiculos_has_conductores as vhc on vhc.vehiculos_idvehiculos = vh.idvehiculos group by vhc.conductores_idconductores,vh.placa, td.documento, emp.numero_identificacion, emp.nombre having count(vhc.conductores_idconductores)>2 order by vh.placa 
